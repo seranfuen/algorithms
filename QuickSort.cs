7f4ec9996ed6721b2b0303c5fb3fdeab489d1335
Sergio Ángel Verbo <seranfuen@gmail.com>
2020-07-05 16:07:44 +0200

    QuickSort implemented

---
+++ QuickSort.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Algorithms
+{
+    public class QuickSort
+    {
+        private readonly Random _random = new Random();
+
+        public int[] Sort(int[] input)
+        {
+            SortAux(input, 0, input.Length);
+            return input;
+        }
+
+        private void SortAux(int[] input, int start, int end)
+        {
+            if (end - start <= 1) return;
+            var pivot = _random.Next(start, end);
+            var pivotPosition = Arrange(input, start, pivot, end);
+
+            SortAux(input, start, pivotPosition);
+            SortAux(input, pivotPosition + 1, end);
+        }
+
+        private int Arrange(int[] input, int start, int pivotPosition, int end)
+        {
+            var i = start + 1;
+            var pivotValue = input[pivotPosition];
+            input[pivotPosition] = input[start];
+            input[start] = pivotValue;
+
+            for (var j = start + 1; j < end; j++)
+                if (input[j] < pivotValue)
+                {
+                    var aux = input[i];
+                    input[i] = input[j];
+                    input[j] = aux;
+                    i++;
+                }
+
+            input[start] = input[i - 1];
+            input[i - 1] = pivotValue;
+
+            return i - 1;
+        }
+    }
+}
--- Tests/QuickSortTests.cs
+++ Tests/QuickSortTests.cs
@@ -1,9 +1,10 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using NUnit.Framework;
 
 namespace Algorithms.Tests
 {
     [TestFixture]
-    public class MergeSortingTests
+    public class QuickSortTests
     {
         [TestCase(new int[] { }, ExpectedResult = new int[] { }, Description = "Fringe case: empty")]
         [TestCase(new[] {5}, ExpectedResult = new[] {5}, Description = "Fringe case: one element")]
@@ -14,12 +15,13 @@ namespace Algorithms.Tests
         [TestCase(new[] {3, 2, 3}, ExpectedResult = new[] {2, 3, 3})]
         [TestCase(new[] {3, 2, 5}, ExpectedResult = new[] {2, 3, 5})]
         [TestCase(new[] {10, 9, 8, 8, 7, 6, 5}, ExpectedResult = new[] {5, 6, 7, 8, 8, 9, 10})]
-        [TestCase(new[] { 0, -1, -2, -3, -4 }, ExpectedResult = new[] { -4, -3, -2, -1, 0 })]
-        [TestCase(new[] { 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100 },
-            ExpectedResult = new[] { 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100 })]
+        [TestCase(new[] {0, -1, -2, -3, -4}, ExpectedResult = new[] {-4, -3, -2, -1, 0})]
+        [TestCase(new[] {5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100},
+            ExpectedResult = new[] {5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100})]
         public int[] Test(int[] input)
         {
-            return new MergeSorting().MergeSort(input);
+            var sort = new QuickSort().Sort(input.ToArray());
+            return sort;
         }
     }
 }
