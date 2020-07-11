a4b17bf8964ef3e204b8702fd6ecbc7d4d814f39
Sergio Ángel Verbo <seranfuen@gmail.com>
2020-07-11 21:03:39 +0200

    Starting with graphs, a breadth-first algorithm for calculating distances
    it's from the root, I will generalize to the distance from any node later

---
+++ GraphDistance.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms
+{
+    public class GraphDistance
+    {
+        public int[] CalculateDistancesFromRoot(int[][] adjacencyGraph)
+        {
+            if (adjacencyGraph?.Length > 0 != true) return new int[0];
+            var visitedNodes = new bool[adjacencyGraph.Length];
+            var distances = InitializeDistances(adjacencyGraph);
+
+            var queue = new Queue<int>();
+            queue.Enqueue(0);
+            visitedNodes[0] = true;
+
+            while (queue.Any())
+            {
+                var node = queue.Dequeue();
+                var nodesToVisit = adjacencyGraph[node];
+                foreach (var nodeToVisit in nodesToVisit)
+                {
+                    if (visitedNodes[nodeToVisit]) continue;
+                    visitedNodes[nodeToVisit] = true;
+                    distances[nodeToVisit] = distances[node] + 1;
+                    queue.Enqueue(nodeToVisit);
+                }
+            }
+
+            return distances;
+        }
+
+        private static int[] InitializeDistances(int[][] adjacencyGraph)
+        {
+            var result = new int[adjacencyGraph.Length];
+            for (var i = 1; i < result.Length; i++)
+                result[i] = int.MaxValue;
+            return result;
+        }
+    }
+}
---
+++ Tests/GraphDistanceTests.cs
@@ -0,0 +1,106 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Algorithms.Tests
+{
+    [TestFixture]
+    public class GraphDistanceTests
+    {
+        [Test]
+        public void Empty_graph_returns_empty_result()
+        {
+            var result = new GraphDistance().CalculateDistancesFromRoot(new int[0][]);
+            result.Length.Should().Be(0);
+        }
+
+        [Test]
+        public void Null_graph_returns_empty_result()
+        {
+            var result = new GraphDistance().CalculateDistancesFromRoot(null);
+            result.Length.Should().Be(0);
+        }
+
+        [Test]
+        public void Only_one_node_distance_0()
+        {
+            var result = new GraphDistance().CalculateDistancesFromRoot(new[] {new int[] { }});
+            result.Length.Should().Be(1);
+            result[0].Should().Be(0);
+        }
+
+        [Test]
+        public void Tests_distances_properly()
+        {
+            var adjacencyGraph = new[]
+            {
+                new[] {1, 2}, // 0
+                new[] {3, 0}, // 1
+                new[] {3, 4, 0}, // 2
+                new[] {5, 1, 2}, // 3
+                new[] {2}, // 4
+                new[] {3, 6}, // 5
+                new[] {5} // 6
+            };
+
+            var result = new GraphDistance().CalculateDistancesFromRoot(adjacencyGraph);
+            result.Length.Should().Be(7);
+            result[0].Should().Be(0);
+            result[1].Should().Be(1);
+            result[2].Should().Be(1);
+            result[3].Should().Be(2);
+            result[4].Should().Be(2);
+            result[5].Should().Be(3);
+            result[6].Should().Be(4);
+        }
+
+        [Test]
+        public void Unidirectional_one()
+        {
+            var adjacencyGraph = new[]
+            {
+                new[] {1}, // 0
+                new[] {2}, // 1
+                new[] {3}, // 2
+                new[] {4}, // 3
+                new[] {5}, // 4
+                new[] {6}, // 5
+                new int[] { } // 6
+            };
+
+            var result = new GraphDistance().CalculateDistancesFromRoot(adjacencyGraph);
+            result.Length.Should().Be(7);
+            result[0].Should().Be(0);
+            result[1].Should().Be(1);
+            result[2].Should().Be(2);
+            result[3].Should().Be(3);
+            result[4].Should().Be(4);
+            result[5].Should().Be(5);
+            result[6].Should().Be(6);
+        }
+
+        [Test]
+        public void With_disconnected_node_its_distance_is_MaxInteger()
+        {
+            var adjacencyGraph = new[]
+            {
+                new[] {1, 2}, // 0
+                new[] {3, 0}, // 1
+                new[] {3, 4, 0}, // 2
+                new[] {5, 1, 2}, // 3
+                new[] {2}, // 4
+                new[] {3}, // 5
+                new[] {5} // 6
+            };
+
+            var result = new GraphDistance().CalculateDistancesFromRoot(adjacencyGraph);
+            result.Length.Should().Be(7);
+            result[0].Should().Be(0);
+            result[1].Should().Be(1);
+            result[2].Should().Be(1);
+            result[3].Should().Be(2);
+            result[4].Should().Be(2);
+            result[5].Should().Be(3);
+            result[6].Should().Be(int.MaxValue);
+        }
+    }
+}
