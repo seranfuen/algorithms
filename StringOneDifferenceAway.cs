2a24f68810c0154c6aa327fd0b469f7bbb995af4
Sergio Ángel Verbo <seranfuen@gmail.com>
2020-07-13 21:01:55 +0200

    One difference only between to strings algorithm

---
+++ StringOneDifferenceAway.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Algorithms
+{
+    public class StringOneDifferenceAway
+    {
+        public bool AreOneDifferenceAway(string a, string b)
+        {
+            if (Math.Abs(a.Length - b.Length) > 1) return false;
+
+            if (a.Length == b.Length)
+                return CheckDifferences(a, b);
+            if (a.Length < b.Length)
+                return CheckInsertion(a, b);
+            if (a.Length > b.Length)
+                return CheckDeletion(a, b);
+            return false;
+        }
+
+        private bool CheckDeletion(string a, string b)
+        {
+            if (a.Length == 0 || b.Length == 0) return true;
+
+            var index2 = 0;
+
+            var difFFound = false;
+            for (var i = 0; i < a.Length; i++)
+            {
+                if (a[i] != b[index2])
+                {
+                    if (difFFound) return false;
+                    difFFound = true;
+                }
+                else
+                {
+                    index2++; 
+                }
+            }
+
+            return true;
+        }
+
+        private bool CheckInsertion(string a, string b)
+        {
+            if (a.Length == 0 || b.Length == 0) return true;
+
+            var index1 = 0;
+     
+            var foundDiff = false;
+
+            for (var i = 0; i < b.Length; i++)
+                if (a[index1] != b[i])
+                {
+                    if (foundDiff) return false;
+
+                    foundDiff = true;
+                }
+                else
+                {
+                    index1++;
+                    if (index1 >= a.Length) return true;
+                }
+
+            return true;
+        }
+
+        private bool CheckDifferences(string a, string b)
+        {
+            var foundDiff = false;
+            for (var i = 0; i < a.Length; i++)
+                if (a[i] != b[i])
+                {
+                    if (foundDiff)
+                        return false;
+
+                    foundDiff = true;
+                }
+
+            return true;
+        }
+    }
+}
---
+++ Tests/StringOneDifferenceAwayTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace Algorithms.Tests
+{
+    [TestFixture]
+    public class StringOneDifferenceAwayTests
+    {
+        [TestCase("Mum", "Mam", ExpectedResult = true)]
+        [TestCase("Mum", "Mm", ExpectedResult = true)]
+        [TestCase("Mum", "am", ExpectedResult = false)]
+        [TestCase("Mum", "Muma", ExpectedResult = true)]
+        [TestCase("Mum", "Mama", ExpectedResult = false)]
+        [TestCase("Mum", "Mad", ExpectedResult = false)]
+        [TestCase("", "A", ExpectedResult = true)]
+        [TestCase("A", "", ExpectedResult = true)]
+        [TestCase("Marke", "Mo", ExpectedResult = false)]
+        [TestCase("Abcd", "bcd", ExpectedResult = true)]
+        [TestCase("Abcd", "aBcd", ExpectedResult = false, Reason = "Case sensitivity")]
+        [TestCase("Acde", "Abcde", ExpectedResult = true)]
+        public bool Test(string a, string b)
+        {
+            return new StringOneDifferenceAway().AreOneDifferenceAway(a, b);
+        }
+    }
+}
